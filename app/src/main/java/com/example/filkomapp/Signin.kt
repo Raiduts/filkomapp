6232418de0a1fbd700fa98f336f7a0c0a9d4f39f
Raiduts <rivaldorakun1253@gmail.com>
2025-06-10 00:01:38 +0700

    First

--- app/src/main/java/com/example/filkomapp/Signin.kt
+++ app/src/main/java/com/example/filkomapp/Signin.kt
@@ -10,7 +10,7 @@ class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signin)
+        setContentView(R.layout.signin)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
